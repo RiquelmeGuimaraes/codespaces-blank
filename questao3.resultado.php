f0b23231430e5cdad9cbdb5a62f066ebdae834e7
RiquelmeGuimaraes <141530352+RiquelmeGuimaraes@users.noreply.github.com>
2023-08-06 01:22:26 +0000

    main e php

--- questao3.resultado.php
+++ questao3.resultado.php
@@ -12,6 +12,46 @@
     <header>
         <h1>Desenvolvimento Web</h1>
     </header>
+    <main>
+        <?php
+        switch ($_GET['Q1']) {
+            case "C":
+                echo ("1 - {$_GET['Q1']} - <i class=\"fa-regular fa-circle-check\"></i><br>");
+                break;
+            default:
+                echo ("1 - {$_GET['Q1']} - <i class=\"fa-regular fa-circle-xmark\"></i><br>");
+                break;
+        }
+
+        switch ($_GET['Q2']) {
+            case "E":
+                echo ("2 - {$_GET['Q2']} - <i class=\"fa-regular fa-circle-check\"></i><br>");
+                break;
+            default:
+                echo ("2 - {$_GET['Q2']} - <i class=\"fa-regular fa-circle-xmark\"></i><br>");
+                break;
+        }
+
+        switch ($_GET['Q3']) {
+            case "A":
+                echo ("3 - {$_GET['Q3']} - <i class=\"fa-regular fa-circle-check\"></i><br>");
+                break;
+            default:
+                echo ("3 - {$_GET['Q3']} - <i class=\"fa-regular fa-circle-xmark\"></i><br>");
+                break;
+        }
+        switch ($_GET['Q4']) {
+            case "B":
+                echo ("4 - {$_GET['Q4']} - <i class=\"fa-regular fa-circle-check\"></i><br>");
+                break;
+            default:
+                echo ("4 - {$_GET['Q4']} - <i class=\"fa-regular fa-circle-xmark\"></i><br>");
+                break;
+        }
+        ?>
+
+        <a href="index.html">Página Inicial</a>
+    </main>
 </body>
 
 </html>
