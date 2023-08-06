0fb9d14f02b8dfda4c010f4af117b2882b2743f7
RiquelmeGuimaraes <141530352+RiquelmeGuimaraes@users.noreply.github.com>
2023-08-06 01:03:26 +0000

    pagina pronta

--- questao03.php
+++ questao03.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Questão 3</title>
 </head>
+
 <body>
-    
+    <h2>Questão 3</h2>
+    <h3> Q1. ipsum dolor sit amet.</h3>
+    <a href="questao.3.2.php?Q1=A">A) Lorem ipsum dolor sit amet.</a><br>
+    <a href="questao.3.2.php?Q1=B">B) Lorem ipsum dolor sit amet.</a><br>
+    <a href="questao.3.2.php?Q1=C" style="color:green;">C) Lorem ipsum dolor sit amet.</a><br>
+    <a href="questao.3.2.php?Q1=D">D) Lorem ipsum dolor sit amet.</a><br>
+    <a href="questao.3.2.php?Q1=E">E) Lorem ipsum dolor sit amet.</a><br><br>
+    <a href="questao03.php">Cancelar</a><br>
+    <a href="index.html">Página Inicial</a>
 </body>
+
 </html>
